7483403ada7d11ad4d5cd64475b5fea419c1269f
Kritigya <ckritigya@gmail.com>
2026-01-03 23:21:20 +0530

    Fix

--- schema.js
+++ schema.js
@@ -13,28 +13,40 @@ module.exports = gql`
     flagged: Boolean
   }
 
- input CreateEmployeeInput {
-  name: String!
-  email: String!
-  password: String!
-  age: Int
-  department: String
-  role: String
-  salary: Int
-  joinDate: String
-}
+  # New type for the logged-in user's profile
+  type Me {
+    id: ID!
+    email: String
+    role: String         # system role
+    name: String
+    age: Int
+    department: String
+    employeeRole: String
+    salary: Int
+    joinDate: String
+  }
 
-input UpdateEmployeeInput {
-  name: String
-  email: String
-  age: Int
-  department: String
-  role: String
-  salary: Int
-  joinDate: String
-  flagged: Boolean
-}
+  input CreateEmployeeInput {
+    name: String!
+    email: String!
+    password: String!
+    age: Int
+    department: String
+    role: String
+    salary: Int
+    joinDate: String
+  }
 
+  input UpdateEmployeeInput {
+    name: String
+    email: String
+    age: Int
+    department: String
+    role: String
+    salary: Int
+    joinDate: String
+    flagged: Boolean
+  }
 
   type EmployeePage {
     data: [Employee]
@@ -56,13 +68,13 @@ input UpdateEmployeeInput {
   type Query {
     employees(page: Int, limit: Int, search: String, sortBy: String, order: String): EmployeePage
     employee(id: ID!): Employee
-    me: User
+    me: Me          # <-- updated to return Me type with all employee fields
   }
 
   type Mutation {
     login(email: String!, password: String!): AuthPayload
-  addEmployee(input: CreateEmployeeInput!): Employee
-updateEmployee(id: ID!, input: UpdateEmployeeInput!): Employee
+    addEmployee(input: CreateEmployeeInput!): Employee
+    updateEmployee(id: ID!, input: UpdateEmployeeInput!): Employee
     deleteEmployee(id: ID!): Employee
     flagEmployee(id: ID!): Employee
     logout: Boolean
